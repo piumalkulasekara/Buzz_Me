419c1ed01da9cbafb7e8bc4dd3f2cee6e329631f
Sashinip <sashinip@zone24x7.com>
2018-03-09 18:43:06 +0530

    google map changes

--- app/src/main/java/com/buzz_me/sashinip/buzz_me_driver/DuringRideActivity.java
+++ app/src/main/java/com/buzz_me/sashinip/buzz_me_driver/DuringRideActivity.java
@@ -1,6 +1,8 @@
 package com.buzz_me.sashinip.buzz_me_driver;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,6 +14,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class DuringRideActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int MY_REQUEST_INT = 177;
     private GoogleMap mMap;
 
     @Override
@@ -37,6 +40,22 @@ public class DuringRideActivity extends FragmentActivity implements OnMapReadyCa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+
+            /*if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, MY_REQUEST_INT);
+            }*/
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(7.8731, 80.7718);
